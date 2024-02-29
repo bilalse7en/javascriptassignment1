af3896d4a79c523b51679721ef5c0de49a406126
Muhammad Bilal <bilal.qsols@gmail.com>
2024-02-29 09:31:14 +0500

    initial

---
+++ app.js
@@ -0,0 +1,102 @@
+// Chapter 1
+
+//Q1
+// alert("Hello Sir/Maddam")
+
+// Q2
+// alert("Error! Please enter a valid password")
+
+// Q3
+// alert("Welcome to JS Land... \nHappy Coding!")
+
+// Q4
+// var helloToUser = "Welcome To JS Land...."
+// var helloCoding = "Hello Coding!"
+
+// alert(helloToUser)
+// alert(helloCoding)
+
+// var hello = "Hello... I can run JS through my web browser's console"
+
+// alert(hello);
+// console.log(hello)
+
+
+// chapter 2
+
+// Q1
+// var UserName = "Bilal"
+// alert("Student Name: "+ UserName)
+
+// Q2
+// var myName = "Muhammad Bilal"
+// alert("Full Name: "+ myName)
+
+// Q3
+// var username = "Muhammad Bilal"
+
+// alert("Hello World!:"+ username)
+
+
+
+// Q4
+// var studentName = "Bilal";
+// var studentAge = "27";
+// var course = "Certified Mobile Application Development";
+
+// // Displaying the data in alert boxes
+// alert("Student Name: " + studentName);
+// alert("Student Age: " + studentAge);
+// alert("Course: " + course);
+
+// Q5
+// var word = "PIZZA";
+// var alertMessage = "";
+
+// for (var i = 0; i < word.length; i++) {
+//     alertMessage += word.slice(0, word.length - i) + "\n";
+// }
+
+// alert(alertMessage);
+
+// Q6
+// var email = "example@example.com";
+// alert("My email address is: " + email);
+
+// Q7
+// var book = "A smarter way to learn javaScript"
+
+// alert(book)
+
+// Q8
+// var hhh = "Yah! I can write HTML Content through JavaScript"
+// document.write(hhh + "<br>");
+
+//Q9
+// var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(design)
+
+
+// Chapter 3
+
+// Q1
+// var age = "I'm 27 years old"
+// alert(age)
+
+// Q2
+// var howMuchVisits = 0
+// howMuchVisits++
+// alert("You are visited "+howMuchVisits)
+
+// Q3
+// var number = 1996
+// var ageYear = "My birth year is " +number
+// document.write(ageYear +  "<br>");
+
+// Q4
+var customerName = "Muhammad Bilal "
+var product = "T-shirt "
+var quantity = "5"
+var storeName = "XYZ Clothing store"
+
+document.write("<b>"+customerName+ "</b>" + "ordered " + "<b>"+product+"</b>" + "(s) on " + storeName+ "<br>");
